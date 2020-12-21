a02d49eb892c7e715752afabe9945b94d71130a4
qiangjishen <qiangjishen@gmail.com>
2020-12-21 09:14:15 +0800

    init

---
+++ go_tourl/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"qjstest/aaa"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+
+	aaa.AddPeter()
+
+	app.Listen(":3000")
+}
